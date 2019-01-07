c611cfed28a5551f62af4c96fe50dc00d20d5617
mrollins <mark_rollins@cable.comcast.com>
2019-01-07 11:41:13 -0800

    Fix daemon support in rtUnicastResolver

--- remote/rtUnicastResolver.cpp
+++ remote/rtUnicastResolver.cpp
@@ -7,20 +7,13 @@
 #include <list>
 #include <queue>
 
-#define WEBSOCKET_PORT_NUMBER 3000  //TODO - add to config
+#define WEBSOCKET_PORT_NUMBER 3001  //TODO - add to config
 
 using namespace uWS;
 using namespace std;
 
 typedef WebSocket<SERVER> WSSocket;
 class Resolver;
-
-class Daemon
-{
-public:
-    Daemon();
-    ~Daemon();    
-};
 
 class WorkerThread
 {
@@ -301,42 +294,9 @@ WSSocket* Resolver::dequeue()
     return socket;
 }
 
-Daemon::Daemon()
-{
-    pid_t pid, sid;
-    pid = fork();
-    if(pid > 0)
-        exit(EXIT_SUCCESS);
-    else
-        exit(EXIT_FAILURE);
-    umask(0);
-    openlog("rtUnicastResolver", LOG_NOWAIT | LOG_PID, LOG_USER);
-    syslog(LOG_NOTICE, "Successfully started daemon-name");
-    sid = setsid();
-    if(sid < 0)
-    {
-        syslog(LOG_ERR, "Could not generate session ID for child process");
-        exit(EXIT_FAILURE);
-    }
-    if((chdir("/")) < 0)
-    {
-        syslog(LOG_ERR, "Could not change working directory to /");
-        exit(EXIT_FAILURE);
-    }
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-}
-
-Daemon::~Daemon()
-{
-   syslog(LOG_NOTICE, "Stopping daemon-name");
-   closelog();
-}
-
 int main()
 {
-    Daemon d;
+    daemon(1,1);
     Resolver r;
     r.run();
     return 1;
